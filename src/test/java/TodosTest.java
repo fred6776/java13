8a6470d4966b711259936abec7fece75b697ab12
Farid Mukhametkhanov <fred6776@yandex.com>
2025-08-11 18:54:46 +0300

    Rework task

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -28,8 +28,37 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+
+    //1) находится несколько задач;
     @Test
-    public void shouldSearchOfDifferentType() {
+    public void shouldSearchMultipleTasks() {
+
+        SimpleTask simpleTask = new SimpleTask(5, "Создать приложение");
+
+        String[] subtasks = {"Создать приложение", "Обновить ОС", "Удалить архив"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Создать приложение",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    // 2) находится ровно одна задача;
+    @Test
+    public void shouldSearchSingleTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
@@ -51,5 +80,32 @@ public class TodosTest {
         Task[] expected = {epic};
         Task[] actual = todos.search("Хлеб");
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    // 3) находится 0 задач, т.е. ни одна задача не подходит.
+    @Test
+    public void shouldSearchNoTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Сыр");
+        Assertions.assertArrayEquals(expected, actual);
+
     }
 }
