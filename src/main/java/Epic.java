28d13970413b6b9555e03ea36e4e4f3ade3bdfbb
Farid Mukhametkhanov <fred6776@yandex.com>
2025-08-11 11:41:16 +0300

    Test verify

---
+++ src/main/java/Epic.java
@@ -0,0 +1,22 @@
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id); // вызов родительского конструктора
+        this.subtasks = subtasks; // заполнение своих полей
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String subtask : subtasks) {
+            if (subtask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/TasksTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    @Test
+    public void shouldMatchesSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean expected = true;
+        boolean actual = simpleTask.matches("Позвонить родителям");
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldMatchesEpic() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean expected = true;
+        boolean actual = epic.matches("Яйца");
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldMatchesMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean expected = true;
+        boolean actual = meeting.matches("Приложение НетоБанка");
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+}
